02d113a7c7be2a127d4e5d659b8b28ff9a97b4d0
Nicholas Domingue <112515521+NichoDA@users.noreply.github.com>
2024-01-03 04:33:01 -0600

    about section done

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
+    const navLinks = document.querySelectorAll('nav a');
 
     window.addEventListener('scroll', function () {
-
         const scrollPosition = window.scrollY;
-        const headerHeight = home.clientHeight-105;
+        const headerHeight = home.clientHeight - 150;
         const homeSectionOffset = document.getElementById('home').offsetTop;
         const opacity = Math.min(1, Math.max(0, (scrollPosition - homeSectionOffset) / (headerHeight / 5)));
-
 
         if (scrollPosition < homeSectionOffset + headerHeight) {
             header.style.backgroundColor = `rgba(51, 51, 51, ${opacity})`;
@@ -20,6 +19,18 @@ document.addEventListener('DOMContentLoaded', function () {
             header.style.backgroundColor = 'rgba(51, 51, 51, 0.8)';
             header.style.opacity = Math.min(1, (scrollPosition - (homeSectionOffset + headerHeight)) / (headerHeight / 5));
         }
+
+        document.querySelectorAll('section').forEach(section => {
+            const sectionId = section.getAttribute('id');
+            const sectionOffset = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (scrollPosition >= sectionOffset && scrollPosition < sectionOffset + sectionHeight) {
+                navLinks.forEach(link => link.classList.remove('active'));
+
+                document.querySelector(`nav a[href="#${sectionId}"]`).classList.add('active');
+            }
+        });
     });
 });
 
@@ -30,6 +41,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
     });
 });
 
@@ -37,15 +51,18 @@ function scrollToAbout() {
     document.querySelector('#about').scrollIntoView({
         behavior: 'smooth'
     });
+
+    navLinks.forEach(link => link.classList.remove('active'));
+    document.querySelector('nav a[href="#about"]').classList.add('active');
+
     header.style.backgroundColor = 'rgba(51, 51, 51, 0.8)';
-    
 }
 
 function hoverArrow(isHovered) {
     const arrowImg = document.getElementById('arrow-img');
     if (isHovered) {
-        arrowImg.src = 'images/downhover.png'; 
+        arrowImg.src = 'images/downhover.png';
     } else {
-        arrowImg.src = 'images/downarrow.png'; 
+        arrowImg.src = 'images/downarrow.png';
     }
 }
