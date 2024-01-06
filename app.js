a023ea1f5e5f6a4fb4f886bf0d8692342d6ca805
Nicholas Domingue <112515521+NichoDA@users.noreply.github.com>
2024-01-06 03:18:49 -0600

    asd

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (sectionId === 'contact' && scrollPosition >= sectionOffset - 390) {
                 navLinks.forEach(link => link.classList.remove('active'));
                 document.querySelector(`nav a[href="#${sectionId}"]`).classList.add('active');
-            } else if (scrollPosition >= sectionOffset -10 && scrollPosition <= sectionOffset + sectionHeight) {
+            } else if (scrollPosition >= sectionOffset -30 && scrollPosition <= sectionOffset + sectionHeight) {
                 navLinks.forEach(link => link.classList.remove('active'));
                 document.querySelector(`nav a[href="#${sectionId}"]`).classList.add('active');
             }
